3329086d0aa21ac22f3b90696c0bc4f9460c704f
ᴍᴅʙᴏᴛ✅ <125588175+chamikara2006@users.noreply.github.com>
2023-03-01 22:27:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'Q2F1OEJEMmY=',
+global.ownername = process.env.OWNER_NAME ===  ? "chamodhmd" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '94715491788' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
